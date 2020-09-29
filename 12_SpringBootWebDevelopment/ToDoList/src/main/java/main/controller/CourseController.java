011a001b0bab77b9bb9cc79772ee8278a617c35b
Alexey.Pestov <alexpestov89@yandex.ru>
2020-09-29 14:35:33 +0300

    Homework 12.3 rework

--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/controller/CourseController.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/controller/CourseController.java
@@ -17,64 +17,65 @@ public class CourseController {
     private CourseRepository courseRepository;
 
     @GetMapping("/courses/")  // сделано бд
-    public List<Course> list()  {
-        return Storage.getCourses(courseRepository);
+    public List<Course> list() {
+        return (List<Course>) courseRepository.findAll();
     }
 
     @PostMapping("/courses/") // сделано бд
-    public int createCourse(Course course)  {
+    public int createCourse(Course course) {
         Course newCourse = courseRepository.save(course);
         return newCourse.getId();
     }
 
     @GetMapping("/courses/{id}") // сделано бд
-    public ResponseEntity get (@PathVariable int id)  {
+    public ResponseEntity get(@PathVariable int id) {
         Optional<Course> optionalCourse = courseRepository.findById(id);
-        if(!optionalCourse.isPresent()) {
+        if (!optionalCourse.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return new ResponseEntity(optionalCourse.get(), HttpStatus.OK);
     }
+
     @PostMapping("/courses/{id}")
-    public ResponseEntity postId (@PathVariable int id) {
+    public ResponseEntity postId(@PathVariable int id) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(null);
     }
 
     @PutMapping("/courses/") // сделано бд
-    public List<Course>  updAllCourses(Course course)  {
+    public List<Course> updAllCourses(Course course) {
         //Storage.updAllCourses(course);
-        Iterable <Course> allCourses = courseRepository.findAll();
-        allCourses.forEach(c -> {c.setName(course.getName()); c.setYear(course.getYear());});
+        Iterable<Course> allCourses = courseRepository.findAll();
+        allCourses.forEach(c -> {
+            c.setName(course.getName());
+            c.setYear(course.getYear());
+        });
         courseRepository.saveAll(allCourses);
-        return Storage.getCourses(courseRepository);
+        return (List<Course>) courseRepository.findAll();
     }
 
     @PutMapping("/courses/{id}")
-    public List<Course> updateCourse(@PathVariable int id, Course course)   {
-        Course optionalCourse = Storage.getCourse(id,courseRepository);
+    public List<Course> updateCourse(@PathVariable int id, Course course) {
+        Course optionalCourse = courseRepository.findById(id).get();
         optionalCourse.setName(course.getName());
         optionalCourse.setYear(course.getYear());
         courseRepository.save(optionalCourse);
-        return Storage.getCourses(courseRepository);
+        return (List<Course>) courseRepository.findAll();
     }
 
     @DeleteMapping("/courses/") // сделано
-    public ResponseEntity deleteCourses()   {
+    public ResponseEntity deleteCourses() {
         courseRepository.deleteAll();
-        if (Storage.getCourses(courseRepository).size() == 0)   {
-            return new ResponseEntity(null,HttpStatus.OK);
+        if (courseRepository.count() == 0) {
+            return new ResponseEntity(null, HttpStatus.OK);
         }
         return null;
     }
 
     @DeleteMapping("/courses/{id}") // сделано
-    public ResponseEntity deleteCourseId(@PathVariable int id)   {
-        Course course = Storage.getCourse(id,courseRepository);
+    public ResponseEntity deleteCourseId(@PathVariable int id) {
+        Course course = courseRepository.findById(id).get();
         courseRepository.delete(course);
-        if (Storage.getCourse(id, courseRepository) == null)  {
-            return ResponseEntity.status(HttpStatus.OK).body(null);
-        }
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
 }
